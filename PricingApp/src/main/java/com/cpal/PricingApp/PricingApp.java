a287671eb77562ffb170e51faa219776839e5f92
chandra Reddy <CReddy@routeone.com>
2018-03-28 18:12:02 -0400

    CARD-01: initial setup

---
+++ PricingApp/src/main/java/com/cpal/PricingApp/PricingApp.java
@@ -0,0 +1,12 @@
+package com.cpal.PricingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricingApp.class, args);
+	}
+}
