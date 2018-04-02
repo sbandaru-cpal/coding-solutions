05ec57d7af94eddf290263630f9bcfaa750486a0
chandra Reddy <creddy.cpal@outlook.com>
2018-04-02 08:01:00 -0400

    CARD-01:  Added resource IT and changed the application setup

--- TaxCalculator/src/test/java/com/cpal/TaxCalculatorServiceTest.java
+++ TaxCalculator/src/test/java/com/cpal/TaxCalculatorServiceTest.java
@@ -1,4 +1,4 @@
-package com.cpal.TaxCalculator;
+package com.cpal;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TaxCalculatorTest {
+public class TaxCalculatorServiceTest {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
